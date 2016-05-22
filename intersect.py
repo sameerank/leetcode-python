19ab178d9e7369187448d5652de24fed0f5703b7
sameerank <skunche@gmail.com>
2016-05-21 18:09:27 -0700

    adds intersect solution

---
+++ intersect.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        nums1.sort()
+        nums2.sort()
+        intersection = []
+        idx1, idx2 = 0, 0
+        while idx1 < len(nums1) and idx2 < len(nums2):
+            if nums1[idx1] == nums2[idx2]:
+                intersection.append(nums1[idx1])
+                idx1 += 1
+                idx2 += 1
+            elif nums1[idx1] < nums2[idx2]:
+                idx1 += 1
+            else:
+                idx2 += 1
+        return intersection
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
