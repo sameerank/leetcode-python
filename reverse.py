f8b1eff573660c32771409c1be002392e90ebed9
sameerank <skunche@gmail.com>
2016-05-26 00:11:51 -0700

    implements a test for reverse

--- reverse.py
+++ reverse.py
@@ -10,3 +10,18 @@ class Solution(object):
         else:
             res = int(x_stringified[::-1])
         return 0 if abs(res) > 2147483650 else res
+
+import unittest
+
+sol = Solution()
+
+class TestReverse(unittest.TestCase):
+
+    def test_example_one(self):
+        self.assertEqual(sol.reverse(123), 321)
+
+    def test_example_two(self):
+        self.assertEqual(sol.reverse(-123), -321)
+
+if __name__ == '__main__':
+    unittest.main()
