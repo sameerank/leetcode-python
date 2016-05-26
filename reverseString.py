2c6ce3785348a1fcd909d94127af30124d2b2328
sameerank <skunche@gmail.com>
2016-05-26 00:18:40 -0700

    implements a test for reverse string

--- reverseString.py
+++ reverseString.py
@@ -5,3 +5,15 @@ class Solution(object):
         :rtype: str
         """
         return s[::-1]
+
+import unittest
+
+sol = Solution()
+
+class TestReverseString(unittest.TestCase):
+
+    def test_example(self):
+        self.assertEqual(sol.reverseString("hello"), "olleh")
+
+if __name__ == '__main__':
+    unittest.main()
