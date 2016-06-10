2f6b8aa850a4d8f915be6fecbc49825eca26f319
sameerank <skunche@gmail.com>
2016-06-10 05:43:55 -0700

    writes tests for isPowerOfFour

--- isPowerOfFour.py
+++ isPowerOfFour.py
@@ -11,3 +11,24 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
+
+import unittest
+
+sol = Solution()
+
+class TestIsPowerOfFour(unittest.TestCase):
+
+    def test_three(self):
+        self.assertEqual(sol.isPowerOfFour(3), False)
+
+    def test_four(self):
+        self.assertEqual(sol.isPowerOfFour(4), True)
+
+    def test_nine(self):
+        self.assertEqual(sol.isPowerOfFour(9), False)
+
+    def test_sixteen(self):
+        self.assertEqual(sol.isPowerOfFour(16), True)
+
+if __name__ == '__main__':
+    unittest.main()
